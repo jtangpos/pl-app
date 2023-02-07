85de1b9ff6d34d9418132c51af1bff5e8a63a0e4
Jonathan Tangpos <jonathan.tangpos@gmail.com>
2023-02-08 03:47:17 +0800

    update theme and new feature

--- src/app/pages/plate-validator/plate-validator.page.ts
+++ src/app/pages/plate-validator/plate-validator.page.ts
@@ -7,23 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlateValidatorPage implements OnInit {
   plateNumber: string;
+  prefix: string;
   lastDigit: any;
   secondLastDigit: any;
   selectedMonth: any;
   selectedWeek: any;
+  selectedRegion: any;
+  firstLetter: any;
 
   constructor() {
     this.selectedMonth = '';
     this.selectedWeek = '';
+    this.selectedRegion = '';
+    this.firstLetter = '';
   }
 
   ngOnInit() {}
 
   validatePlate() {
+
+    if(!this.plateNumber || !this.prefix) return;
+    
     this.lastDigit = this.getLastDigit(this.plateNumber);
     this.secondLastDigit = this.getSecondLastDigit(this.plateNumber);
     this.selectedMonth = this.getMonthSchedule(this.lastDigit);
     this.selectedWeek = this.getWeekSchedule(this.secondLastDigit);
+
+    this.firstLetter = this.getFirstCharacter(this.prefix);
+    this.selectedRegion = this.getRegion(this.firstLetter);
+  }
+
+  letterOnlyValidation(event:any) {
+    const pattern  = /^[a-zA-Z]+$/;
+    let inputChar = String.fromCharCode(event.charCode);
+
+    if (!pattern.test(inputChar)) {
+      // invalid character, prevent input
+      event.preventDefault();
+    }
+
+  }
+  numberOnlyValidation(event: any) {
+    const pattern = /[0-9.,]/;
+    let inputChar = String.fromCharCode(event.charCode);
+
+    if (!pattern.test(inputChar)) {
+      // invalid character, prevent input
+      event.preventDefault();
+    }
+  }
+
+  getFirstCharacter(str) {
+    return str.slice(0,1);
   }
 
   getLastDigit(str) {
@@ -32,6 +67,40 @@ export class PlateValidatorPage implements OnInit {
 
   getSecondLastDigit(str) {
     return str.slice(-2, -1);
+  }
+
+  getRegion(letter) {
+    letter = letter.toUpperCase();
+    const regionNames = {
+      'A': '',
+      'B': 'Region 2 (Cagayan Valley)',
+      'C': 'Region 3 (Central Luzon)',
+      'D': 'Region 4A (CALABARZON)',
+      'E': 'Region 5 (Bicol Region)',
+      'F': 'Region 6 (Western Visayas)',
+      'G': 'Region 7 (Central Visayas)',
+      'H': 'Region 8 (Eastern Visayas)',
+      'I': 'Region 9 (Ilocos Region)',
+      'J': 'Region 10 (Zamboanga Peninsula)',
+      'K': 'Region 10 (Northern Mindanao)',
+      'L': 'Region 11 (Davao Region)',
+      'M': 'Region 12 (SOCCSKSARGEN)',
+      'N': 'National Capital Region (NCR)',
+      'O': 'Bangsamoro Autonomous Region in Muslim Mindanao (BARMM)',
+      'P': '',
+      'Q': '',
+      'R': '',
+      'S': '',
+      'T': '',
+      'U': '',
+      'V': 'Region 4B (MIMAROPA)',
+      'W': '',
+      'X': '',
+      'Y': 'Cordillera Administrative Region (CAR)',
+      'Z': 'Region 13 (Caraga)'
+    }
+
+    return regionNames[letter];
   }
 
   getMonthSchedule(month) {
@@ -53,16 +122,16 @@ export class PlateValidatorPage implements OnInit {
 
   getWeekSchedule(week) {
     const weekSchedules = {
-      1: '1st Week',
-      2: '1st Week',
-      3: '1st Week',
-      4: '2nd Week',
-      5: '2nd Week',
-      6: '2nd Week',
-      7: '3rd Week',
-      8: '3rd Week',
-      9: '4th Week',
-      0: '4th Week'
+      1: '1st - 7th Day of the Month',
+      2: '1st - 7th Day of the Month',
+      3: '1st - 7th Day of the Month',
+      4: '8th - 14th Day of the Month',
+      5: '8th - 14th Day of the Month',
+      6: '8th - 14th Day of the Month',
+      7: '15th - 21st Day of the Month',
+      8: '15th - 21st Day of the Month',
+      9: '22nd - last Day of the Month',
+      0: '22nd - last Day of the Month'
     };
 
     return weekSchedules[week];
